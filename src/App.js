4b158e67f816a4af999ed374159113ca9a4a208f
azer8799 <65220500+azer8799@users.noreply.github.com>
2020-12-22 14:39:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React,{ useState } from "react"
+import Header from "./Header"
+import "./Notes.css";
+import Footer from "./Footer.js"
+import Note from "./Note"
+import Textarea from "./Textarea";
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <Textarea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
